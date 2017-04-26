c1bceb85eb2d04cc353d702c08b45d5b08122ef5
imhansonlan <imhansonlan@gmail.com>
2017-04-26 22:08:08 +0800

    add new file

---
+++ indicator-gcolor2.py
@@ -0,0 +1,67 @@
+# This code is an example for a tutorial on Ubuntu Unity/Gnome AppIndicators:
+# http://candidtim.github.io/appindicator/2014/09/13/ubuntu-appindicator-step-by-step.html
+
+import os
+import signal
+import time
+import subprocess
+
+from gi.repository import Gtk as gtk
+from gi.repository import AppIndicator3 as appindicator
+# from gi.repository import Notify as notify
+
+
+APPINDICATOR_ID = 'myappindicator'
+
+
+def main():
+    # icon = gtk.IconTheme.get_default().load_icon('gcolor2', 22, 0)
+    indicator = appindicator.Indicator.new(APPINDICATOR_ID, 'gcolor-indicator', appindicator.IndicatorCategory.SYSTEM_SERVICES)
+    indicator.set_status(appindicator.IndicatorStatus.ACTIVE)
+    indicator.set_menu(build_menu())
+    # notify.init(APPINDICATOR_ID)
+    gtk.main()
+
+
+def build_menu():
+    menu = gtk.Menu()
+    item_show = gtk.MenuItem('Show Color Picker')
+    item_show.connect('activate', show)
+    menu.append(item_show)
+
+    item_hide = gtk.MenuItem('Hide')
+    item_hide.connect('activate', hide)
+    menu.append(item_hide)
+
+    item_quit = gtk.MenuItem('Quit')
+    item_quit.connect('activate', quit)
+    menu.append(item_quit)
+    menu.show_all()
+    return menu
+
+
+def show(_):
+    # notify.Notification.new("<b>show</b>", str(output), None).show()
+    p = subprocess.Popen('pidof gcolor2', shell=True, stderr=subprocess.PIPE, stdout=subprocess.PIPE)
+    output = p.communicate()[0]
+    if output != '':
+        os.system('wmctrl -a gcolor2')
+    else:
+        os.system('start-stop-daemon --exec /usr/bin/gcolor2 --name gcolor2 --start --background &>/dev/null')
+        time.sleep(.3)
+
+    os.system('wmctrl -a gcolor2 -e "0,500,100,0,0"')
+
+
+def hide(_):
+    os.system('pkill gcolor2 &>/dev/null')
+
+
+def quit(_):
+    # notify.uninit()
+    gtk.main_quit()
+
+
+if __name__ == "__main__":
+    signal.signal(signal.SIGINT, signal.SIG_DFL)
+    main()
